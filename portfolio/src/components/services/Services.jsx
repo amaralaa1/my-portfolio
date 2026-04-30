0643e5e181461a04850462d26bd4344368c9ffd9
Ismail Alaa <ismaeelalaa63@gmail.com>
2026-04-30 13:32:33 +0300

    fix services component

--- portfolio/src/components/services/Services.jsx
+++ portfolio/src/components/services/Services.jsx
@@ -10,65 +10,37 @@ function Services() {
       <div className="top_section">
         <h2>Services</h2>
       </div>
- <div className="container container_services">
-  <article className='card'>
-  <MdDesignServices className='icon'/>
-  <h3>Web Design</h3>
-  <p className='text-light'>
-    I create clean and modern UI designs that focus on usability, structure, and a good user experience across all devices.
-  </p>
-</article>
 
-<article className='card'>
-  <IoIosRocket className='icon'/>
-  <h3>Fast Performance</h3>
-  <p className='text-light'>
-    I build optimized and responsive web applications that load quickly and run smoothly on all devices and browsers.
-  </p>
-</article>
+      <div className="container container_services">
 
-<article className='card'>
-  <FaCode className='icon'/>
-  <h3>Clean Code</h3>
-  <p className='text-light'>
-    I write clean, reusable, and maintainable code following best practices to make projects scalable and easy to improve.
-  </p>
-</article>
+        <article className='card'>
+          <MdDesignServices className='icon'/>
+          <h3>Web Design</h3>
+          <p className='text-light'>
+            I create clean and modern UI designs that focus on usability, structure, and a good user experience across all devices.
+          </p>
+        </article>
 
+        <article className='card'>
+          <IoIosRocket className='icon'/>
+          <h3>Fast Performance</h3>
+          <p className='text-light'>
+            I build optimized and responsive web applications that load quickly and run smoothly on all devices and browsers.
+          </p>
+        </article>
 
+        <article className='card'>
+          <FaCode className='icon'/>
+          <h3>Clean Code</h3>
+          <p className='text-light'>
+            I write clean, reusable, and maintainable code following best practices to make projects scalable and easy to improve.
+          </p>
+        </article>
 
-
-
-
-
-
-
-  {/* <article className='card'>
-    < MdDesignServices  className='icon'/>
-    <h3>Web Design</h3>
-    <p className='text-light'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed facilis nam architecto.</p>
-  </article>
-
-  <article className='card'>
-    < IoIosRocket  className='icon'/>
-    <h3>Fast Performance</h3>
-        <p className='text-light'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed facilis nam architecto.</p>
-  </article>
-
-  <article className='card'>
-    < FaCode  className='icon'/>
-    <h3>Clean code</h3>
-        <p className='text-light'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed facilis nam architecto.</p>
-  </article> */}
-
-
- </div>
+      </div>
 
     </section>
   );
- }
-
-
-
+}
 
 export default Services
