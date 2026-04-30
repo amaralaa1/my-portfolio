f6c070553a8113239e4412b4b752ea6075869179
Ammar Alaa <amaralaaa209@gmail.com>
2026-04-30 18:47:37 +0300

    fix vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Nav from './components/nav/Nav'
 import Home from './components/home/Home'
 import About from './components/about/About'
@@ -29,35 +28,3 @@ function App() {
 }
 
 export default App
-=======
-import Nav from './components/nav/Nav'
-import Home from './components/home/Home'
-import About from './components/about/About'
-import Skills from './components/skills/Skills'
-import Services from './components/services/Services'
-import Projects from './components/projects/Projects'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
-
-
-function App() {
-  
-
-  return (
-   <>
-<Nav/>
-<Home/>
-<About/>
-<Skills/>
-<Services/>
-<Projects/>
-<Contact/>
-<Footer/>
-
-  
-   </>
-  )
-}
-
-export default App
->>>>>>> 56c640a1139e40f47c72dd26eba207d1c20ffbf0
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -9,15 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> 56c640a1139e40f47c72dd26eba207d1c20ffbf0
