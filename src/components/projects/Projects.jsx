59d82be2dd6ae0953279e34445a1289bc87facc6
ismail-alaa <ismaeelalaa63@gmail.com>
2026-07-01 12:24:01 +0300

    Update portfolio projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,49 +5,76 @@ import IMG2 from '../../assets/GYM.png'
 import IMG3 from '../../assets/bezel.png'
 import IMG4 from '../../assets/weather.png'
 import IMG5 from '../../assets/bookmarker.png'
-import IMG6 from '../../assets/note app.png'
+import IMG6 from '../../assets/dinner.png'
+import IMG7 from '../../assets/contact_hub.png'
+import IMG8 from '../../assets/COSMOS - Space Dashboard.png'
+import IMG9 from '../../assets/note app.png'
 import { FaGithub } from 'react-icons/fa'
 
 const PortfolioData = [
   {
     id: 1,
     image: IMG1,
-    title: 'HTML.CSS 1',
+    title: 'Mealify Restaurant',
     github: 'https://github.com/amaralaa1/Mealify',
     demo: 'https://mealify-wine-phi.vercel.app/'
   },
   {
     id: 2,
     image: IMG2,
-    title: 'HTML.CSS 2',
+    title: 'Modern Gym Website',
     github: 'https://github.com/amaralaa1/GYM',
     demo: 'https://gym-lilac-ten.vercel.app/'
   },
   {
     id: 3,
     image: IMG3,
-    title: 'BootStrap',
+    title: 'Bezel Landing Page',
     github: 'https://github.com/amaralaa1/BEZEL',
     demo: 'https://bezel-two.vercel.app/'
   },
   {
     id: 4,
     image: IMG4,
-    title: 'Java Script',
+    title: 'Weather App',
     github: 'https://github.com/amaralaa1/weather',
     demo: 'https://weather-jet-mu.vercel.app/'
   },
   {
     id: 5,
     image: IMG5,
-    title: 'Java Script',
+    title: 'Bookmark Manager',
     github: 'https://github.com/amaralaa1/boomarker',
     demo: 'https://boomarker.vercel.app/'
   },
+
   {
     id: 6,
     image: IMG6,
-    title: 'React JS',
+    title: 'What Is For Dinner',
+    github: 'https://github.com/amaralaa1/What-s-for-Dinner',
+    demo: 'https://what-s-for-dinner-beta.vercel.app/'
+  },
+   {
+    id: 7,
+    image: IMG7,
+    title: 'Contact Hub',
+    github: 'https://github.com/amaralaa1/ContactHub',
+    demo: 'https://contact-hub-eta.vercel.app/'
+  },
+
+   {
+    id: 8,
+    image: IMG8,
+    title: 'Space Explorer Dashboard',
+    github: 'https://amaralaa1.github.io/COSMOS---Space-Dashboard-/',
+    demo: 'https://cosmos-space-dashboard-eta.vercel.app/'
+  },
+
+  {
+    id: 9,
+    image: IMG9,
+    title: 'NoteAPP',
     github: 'https://github.com/amaralaa1/notapp',
     demo: 'https://notapp-ten.vercel.app/login'
   }
